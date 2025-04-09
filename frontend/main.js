7d7e23c6e805393a26bb2e6ade2e0aaf6ffdff41
Diego Juta <diegojuta@Laneses-MacBook-Pro.local>
2025-04-09 14:51:16 +0100

    Updates to resume, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,15 @@
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(funcctionApi).then(response => {
+        return response.json();
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("count").innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+    });
+    return count;
+}
